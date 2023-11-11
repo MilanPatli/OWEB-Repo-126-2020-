c12cd1b361d798ea3a614b3750b94ac4251bf160
Milan <milanpatlidjankovski@gmail.com>
2023-11-11 13:59:07 +0100

    Update

---
+++ proektnaZadaca1/script.js
@@ -0,0 +1,12 @@
+document.getElementById("button1").addEventListener("click", function () {
+    window.location.href = "newPage.html"; // Navigate to the new HTML file
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+    window.location.href = "homePage.html"; // Navigate to the new HTML file
+});
+
+document.getElementById("button3").addEventListener("click", function () {
+    window.location.href = "contactsPage.html"; // Navigate to the new HTML file
+ });
+ 
