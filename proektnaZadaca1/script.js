7e0c66357e7d955cfd588ba5996a4c9f03364d27
Milan <milanpatlidjankovski@gmail.com>
2023-11-15 21:48:35 +0100

    +

--- proektnaZadaca1/script.js
+++ proektnaZadaca1/script.js
@@ -1,3 +1,6 @@
+var flag = 1;
+var array = [];
+
 document.getElementById("button1").addEventListener("click", function () {
     window.location.href = "newPage.html"; // Navigate to the new HTML file
 });
@@ -13,4 +16,25 @@ document.getElementById("button3").addEventListener("click", function () {
     
  function toggleSelection(img) {
     img.classList.toggle("selected");
-}
+    var element = document.getElementById('order');
+  
+    array.forEach(e => {
+      if(e == img){
+        element.removeChild(document.getElementById(img.src));
+      }
+      else {
+        flag=1;
+      }
+    });
+  
+    if(flag==1){
+        array.push(img);
+        var copy = document.createElement('img');
+        copy.src = img.src;
+        copy.style.width = "20px";  //zgolemi
+        copy.id = img.src;
+        element.appendChild(copy);
+        flag=0;
+    }
+  }
+
